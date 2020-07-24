17879e58d423f111d40e1ae073097d694d752b28
Duo Hong <hong.du@husky.neu.edu>
2020-07-24 18:46:10 -0400

    set up Express s and added server.js content

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 // Put your server side JS here ... or don't you can add your own file if you'd like
+
+const express = require('express');
+const app = express();
+
+// Allow CORS from different origin, namely localhost:3000
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.listen(process.env.PORT || 8080);
+
