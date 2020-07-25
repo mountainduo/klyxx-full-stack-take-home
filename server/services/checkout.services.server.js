7dd5a4f7526878fce750238ce6700a14d217e83b
Duo Hong <hong.du@husky.neu.edu>
2020-07-24 21:04:37 -0400

    inventory controller require in server.js

---
+++ server/services/checkout.services.server.js
@@ -0,0 +1,17 @@
+let checkoutInfo = {
+    "_id": "",
+    "first": "",
+    "last": "",
+    "address": "",
+    "email": "",
+    "phone": "",
+    "card": "",
+    "expiration_date": "",
+    "cvv": "",
+};
+
+getCheckoutInfo = () => checkoutInfo;
+
+submitCheckout = (info) => {
+    checkoutInfo = info;
+}
